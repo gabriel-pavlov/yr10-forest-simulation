9339f8dbed6e07f0841d1e9bc1b2c5dae863dfe9
gabriel-pavlov <66558926+gabriel-pavlov@users.noreply.github.com>
2020-06-07 16:42:31 +0100

    syrup box calcualtion complete, improved report view

--- js/simulation.js
+++ js/simulation.js
@@ -21,7 +21,8 @@ function clearState() {
                 tapped: 0
             },
             nests: 0,
-            trucks: 0
+            trucks: 0,
+            boxes: 0
         
         }]
     };
@@ -31,6 +32,10 @@ function runSimulation() {
 
     clearState();
 
+    global.pinesInOneTruck = 35;
+    global.oaksInOneTruck = 15;
+    global.syrupInMaple = 1.5;
+    global.syrupInOneBox = 0.5;
     global.amountOfYears = $('#amountOfYears').val();
     global.forestSize = $('#forestSize').val();
     global.proportionOfOakTrees = $('#proportionOfOakTrees').val();
@@ -61,16 +66,21 @@ function runSimulation() {
         '<li>number of Pine trees is ' + global.pineSize + '</li>' +
         '<li>every ' + global.doveSize + ' trees there will be a dove nests</li>' +
         '<li>simulation will run for ' + global.amountOfYears + ' years</li>' + 
+        '<li>' + global.pinesInOneTruck + ' pines fit in one truck</li>' + 
+        '<li>' + global.oaksInOneTruck + ' oaks fit in one truck</li>' +
+        '<li>' + global.syrupInMaple + ' litres produced by a maple tree and ' + global.syrupInOneBox + ' litres fit in one box</li>' +
+
         '</ul>';
 
         var report = '<br/><br/>Report:<br/><table class="table">' +
-            '<tr><th>Year</th><th>Oaks (start year)</th><th>Oaks (deseased)</th><th>Oaks (harvested)</th><th>Pines (start year)</th><th>Pines (deseased)</th><th>Pines (harvested)</th><th>Maples (start year)</th><th>Maples (deseased)</th><th>Maples (harvested)</th><th>Maples (tapped)</th><th>Nests (unharvestable)</th><th>Trucks </th></tr>';
+            '<tr><th rowspan=2>Year</th></tr><th colspan=3>Oaks</th><th colspan=3>Pines</th><th colspan=4>Maples</th><th>Nests</th><th rowspan=2>Trucks</th><th rowspan=2>Boxes</th>' +
+            '<tr><th></th><th>start year</th><th>deseas-ed</th><th>harves-ted</th><th>start year</th><th>deseas-ed</th><th>harves-ted</th><th>start year</th><th>deseas-ed</th><th>harves-ted</th><th>tapped</th><th>unharves-table</th></tr>';
         for (var i = 0; i < global.years.length; i++) {
             var yearStats = global.years[i];
             report += '<tr><td>' + (i + 1) + '</td><td>' + yearStats.oaks.quantity + '</td><td>' + yearStats.oaks.diseased + '</td><td>' + yearStats.oaks.harvested + '</td>' +
                                                 '<td>' + yearStats.pines.quantity + '</td><td>' + yearStats.pines.diseased + '</td><td>' + yearStats.pines.harvested + '</td>' + 
                                                 '<td>' + yearStats.maples.quantity + '</td><td>' + yearStats.maples.diseased + '</td><td>' + yearStats.maples.harvested + '</td><td>' + yearStats.maples.tapped + '</td>' +
-                                                '<td>' + yearStats.nests + '</td>' + '<td>' + yearStats.trucks + '</td>' +
+                                                '<td>' + yearStats.nests + '</td>' + '<td>' + yearStats.trucks + '</td>' + '<td>' + yearStats.boxes + '</td>' +
                                                 '</tr>';
         }
         report += '</table>';
@@ -105,6 +115,12 @@ function treeObject(kind, age, diseased = false) {
 
 function calculateTrucks(numberOfTrees, oneTruckLoad){
     return numberOfTrees % oneTruckLoad != 0? Math.ceil(numberOfTrees / oneTruckLoad) : Math.floor(numberOfTrees / oneTruckLoad);
+
+}
+
+function calculateBoxes(numberOfTrees, syrupPerTree, oneBoxLoad){
+    var totalSyrup = numberOfTrees * syrupPerTree;
+    return totalSyrup % oneBoxLoad != 0? Math.ceil(totalSyrup / oneBoxLoad) : Math.floor(totalSyrup / oneBoxLoad);
 
 }
 
@@ -219,8 +235,9 @@ function simulateOneYear(year) {
     global.years[year].pines.harvested = pinesHarvested;
     global.years[year].oaks.harvested = oaksHarvested;
     global.years[year].nests = couldNotHarvestDueToNest;
-    global.years[year].trucks = calculateTrucks(pinesHarvested, 35) + calculateTrucks(oaksHarvested, 15);
+    global.years[year].trucks = calculateTrucks(pinesHarvested, global.pinesInOneTruck) + calculateTrucks(oaksHarvested, global.oaksInOneTruck);
     global.years[year].maples.tapped = maplesTapped;
+    global.years[year].boxes = calculateBoxes(maplesTapped, global.syrupInMaple, global.syrupInOneBox);
 
 
 
@@ -262,7 +279,8 @@ function simulateOneYear(year) {
             tapped: 0
         },
         nests: 0,
-        trucks: 0
+        trucks: 0,
+        boxes: 0
     });
 }
 
